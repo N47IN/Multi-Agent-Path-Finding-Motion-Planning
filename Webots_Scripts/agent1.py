f52a4ee6e55eee41c9a0b83786ac809dca443e0e
Navin <n47insriram@gmail.com>
2024-03-27 01:43:46 +0530

    path-smoothing-and-pure-pursuit

--- Webots_Scripts/agent1.py
+++ Webots_Scripts/agent1.py
@@ -1,12 +1,19 @@
 # Talks at channel 1
 from controller import Robot, GPS, Motor, Keyboard
+from controller import InertialUnit
 from global_planner import RRT_planner
 import numpy as np
 from Comms import comms
+from ccma import CCMA
+from pure_pursuit import PP
 # create the Robot instance.
 robot = Robot()
 keyboard= Keyboard()
-g_planner = RRT_planner("Binary_Mask.png")
+
+w_ma = 4
+w_cc = 2
+
+Yaw = robot.getDevice("inertial unit")
 right_motor = robot.getDevice("right wheel motor")
 left_motor = robot.getDevice("left wheel motor")
 agent2 = robot.getDevice("receiver_agent2")
@@ -16,9 +23,14 @@ emitter = robot.getDevice("emitter")
 gps = robot.getDevice("gps")
 gps.enable(10)
 keyboard.enable(10)
+Yaw.enable(10)
 admin.enable(10)
 agent2.enable(10)
 agent3.enable(10)
+
+comms = comms(agent2= agent2,agent3=agent3, Admin=admin)
+g_planner = RRT_planner("Binary_Mask.png")
+ccma = CCMA(w_ma, w_cc, distrib="hanning")
 
 right_motor.setPosition(100)
 left_motor.setPosition(100)
@@ -32,7 +44,6 @@ command ={
    ord("D"):[0,3]
    }
 
-comms = comms(agent2= agent2,agent3=agent3, Admin=admin)
 path = False
 
 def broadcast(data):
@@ -40,17 +51,31 @@ def broadcast(data):
     message = message.tobytes()
     emitter.send(message)
     
+def steer(data,v):
+    speed = data*v
+    right_motor.setVelocity(-speed)
+    left_motor.setVelocity(speed)
+
+def move(v):
+    right_motor.setVelocity(-v)
+    left_motor.setVelocity(v)
+    
 def rnd(number, precision=4):
     if isinstance(number, (int, float)):
         return round(number, precision)
     if isinstance(number , np.ndarray):
         return np.round(number, precision)
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
+    
+time = 0
+v = 0
+
+
 while robot.step(timestep) != -1:
     position = gps.getValues()
     broadcast(position)
-    #print(position)
+    yaw = Yaw.getRollPitchYaw()
+    yaw = yaw[2] 
+
     
     if admin.getQueueLength()>0 and path==False:
         goal = comms.getAdmin()[0:2]
@@ -58,9 +83,15 @@ while robot.step(timestep) != -1:
         print("goal Agent1 :",goal)
         g_planner.setStart(position[0:2])
         g_planner.setGoal(goal)
-        global_path = g_planner.RRT()
+        global_path = np.asarray(g_planner.RRT())
+        g_plan_smoothed = ccma.filter(global_path, cc_mode=False)
         path = True
-        #print(global_path)
+        tracker = PP(g_plan_smoothed,yaw)
+        print(global_path)
         
-
-# Enter here exit cleanup code.
+    if path == True:
+        while np.linalg.norm(position[0:2] - goal) < 0.05 :
+            v, steering_angle, time = PP.execute(position[0], position[1], yaw, v ,time)
+            steer(steering_angle,v)
+            move(v)
+        
